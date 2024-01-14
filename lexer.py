454179154f9f822d9eceadf9c0262b45539599ee
Jaan Soulier <jaansoulier@gmail.com>
2024-01-14 01:48:58 -0500

    Started on lexer

--- lexer.py
+++ lexer.py
@@ -1,3 +1,5 @@
+import re
+
 from lexer_error import LexerError
 from query import Query
 from query_error import QueryError
@@ -5,4 +7,26 @@ from table import Table
 from table_error import TableError
 
 class Lexer:
-    ''''''
+    '''
+    A parser for tables and queries.
+    Consists of:
+        - tables (Table)
+        - queries (Query)
+    Parses a document and extracts tables and queries.
+    Each query is computed immediately to allow for the reassigning of table
+    names later in the document.
+    '''
+
+    def compute(self, string):
+        pass
+
+    @staticmethod
+    def is_table(string):
+        ''' Check if string looks like a table header. '''
+        pattern = re.compile(r'.*=.*\(.*\).*\{.*')
+        return bool(pattern.match(string))
+
+    @staticmethod
+    def is_query(string, names):
+        ''' Check if string looks like a query. '''
+        return any(name in string for name in names)
--- test_lexer.py
+++ test_lexer.py
@@ -3,6 +3,25 @@ import unittest
 from lexer_error import LexerError
 from lexer import Lexer
 
-class TestLexer:
+class TestLexer(unittest.TestCase):
 
-    pass
+    def test_is_table(self):
+        self.assertTrue(Lexer.is_table('=(){'))
+        self.assertTrue(Lexer.is_table(' = ( )  { '))
+        self.assertTrue(Lexer.is_table('string=(string){string'))
+        self.assertTrue(Lexer.is_table(' string = (string) { string'))
+        self.assertFalse(Lexer.is_table('(){'))
+        self.assertFalse(Lexer.is_table('(=){'))
+        self.assertFalse(Lexer.is_table('({)='))
+        self.assertFalse(Lexer.is_table('=({'))
+
+    def test_is_query(self):
+        self.assertTrue(Lexer.is_query('string', ['string']))
+        self.assertTrue(Lexer.is_query(' string', ['string']))
+        self.assertTrue(Lexer.is_query('string ', ['string']))
+        self.assertTrue(Lexer.is_query('stringstring', ['string']))
+        self.assertTrue(Lexer.is_query('select ID > 2 (string)', ['string']))
+        self.assertTrue(Lexer.is_query(' select ID> 2(string )', ['string']))
+        self.assertFalse(Lexer.is_query('', ['string']))
+        self.assertFalse(Lexer.is_query('select ', ['string']))
+        self.assertFalse(Lexer.is_query(' select ID> 2()', ['string']))
